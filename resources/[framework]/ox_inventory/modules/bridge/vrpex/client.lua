ac955016b08aa11d3c82ef398c4129c441c46ab0
Swellington Soares <qb.corebr@gmail.com>
2024-08-31 02:14:25 -0300

    feature: added ox_inventory

---
+++ resources/[framework]/ox_inventory/modules/bridge/vrpex/client.lua
@@ -0,0 +1,38 @@
+
+
+local Weapon = require 'modules.weapon.client'
+
+AddStateBagChangeHandler('isLoggedIn', ('player:%s'):format(cache.serverId), function(_, _, value)
+    if not value then client.onLogout() end
+end)
+
+AddStateBagChangeHandler('handcuffed', ('player:%s'):format(cache.serverId), function(_, _, value)
+    LocalPlayer.state:set('invBusy', value, false)
+    if not value then return end
+    Weapon.Disarm()
+end)
+
+
+RegisterNetEvent('vRP:client:UpdateGroups', function (groups)
+    lib.print.info('GROUPS', groups)        
+end)
+
+---@diagnostic disable-next-line: duplicate-set-field
+function client.setPlayerStatus(values)
+    lib.print.info('setPlayerStatus', values)
+    -- for name, value in pairs(values) do
+    --     -- Thanks to having status values setup out of 1000000 (matching esx_status's standard)
+    --     -- we need to awkwardly change the value
+    --     if value > 100 or value < -100 then
+    --         -- Hunger and thirst start at 0 and go up to 100 as you get hungry/thirsty (inverse of ESX)
+    --         if (name == 'hunger' or name == 'thirst') then
+    --             value = -value
+    --         end
+
+    --         value = value * 0.0001
+    --     end
+
+    --     ---@diagnostic disable-next-line: undefined-global
+    --     player.addStatus(name, value)
+    -- end
+end
---
+++ resources/[framework]/ox_inventory/modules/bridge/vrpex/server.lua
@@ -0,0 +1,122 @@
+lib.load('@vrp.lib.utils', _G)
+local Proxy = lib.require('@vrp.lib.Proxy')
+local vRP = Proxy.getInterface('vRP')
+
+local Inventory = require 'modules.inventory.server'
+
+local function reorderGroups(groups)
+	local rr = {}
+	for k, v in next, groups or {} do
+		if type(v) == "table" and v?.rank then
+			rr[k] = v.rank
+		end
+	end
+	return rr
+end
+
+local function setCharacterInventory(source, character)
+	if not character then return end
+	local player = {
+		identifier = character.id,
+		name = ("%s %s"):format(character.firstname, character.lastname),
+		user_id = character.user_id,
+		license = character.license,
+		dateofbirth = os.date("%d/%m/%Y", character.birth_date // 1000),
+		groups = reorderGroups(character?.datatable?.groups),
+		sex = character.gender or 'M',
+		source = source
+	}
+	server.setPlayerInventory(player)
+	lib.print.info(character?.money)
+	Inventory.SetItem(source, "money", character?.money?.wallet or 0)
+end
+
+AddEventHandler('vrp:login', function(source, user_id)
+	setCharacterInventory(source, vRP.getPlayerTable(user_id))
+end)
+
+
+AddEventHandler('vRP:PlayerMoneyUpdate', function (user_id, amount, moneytype)
+	if moneytype ~= 'wallet' then return end
+	local src = vRP.getUserSource(user_id)
+	if not src then return end
+	Inventory.SetItem(src, 'money', amount)
+end)
+
+--@diagnostic disable-next-line: duplicate-set-field
+function server.setPlayerData(player)
+	lib.print.info('setPlayerData', player)
+	local user_id = vRP.getUserId(player.source)
+	if user_id then
+		local groups = vRP.getUserGroups(user_id)
+		player.groups = reorderGroups(groups)
+	end
+	return player
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+function server.hasLicense(inv, name)
+	-- local player = Ox.GetPlayer(inv.id)
+
+	-- if not player then return end
+
+	-- return player.getLicense(name)
+	return false
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+function server.buyLicense(inv, license)
+	-- local player = Ox.GetPlayer(inv.id)
+
+	-- if not player then return end
+
+
+	-- if player.getLicense(license.name) then
+	-- 	return false, 'already_have'
+	-- elseif Inventory.GetItemCount(inv, 'money') < license.price then
+	-- 	return false, 'can_not_afford'
+	-- end
+
+	-- Inventory.RemoveItem(inv, 'money', license.price)
+	-- player.addLicense(license.name)
+
+	-- return true, 'have_purchased'
+	return false
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+function server.isPlayerBoss(playerId, group, grade)
+	return vRP.isGroupGradeBoss(group, grade)
+end
+
+---@param entityId number
+---@return number | string
+---@diagnostic disable-next-line: duplicate-set-field
+function server.getOwnedVehicleId(entityId)
+	return GetVehicleNumberPlateText(entityId)
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+function server.syncInventory(inv)
+	local accounts = Inventory.GetAccountItemCounts(inv)
+	if not accounts then return end
+	for account, amount in pairs(accounts) do
+		account = account == 'money' and 'wallet' or account
+		if vRP.getMoney(inv.player.user_id, account) ~= amount then
+			vRP.setMoney(inv.player.user_id, amount, account, ('Sync %s with inventory'):format(account), false)
+		end
+	end
+end
+
+-- AddEventHandler('vRP:playerLeave', function(_, source)
+-- 	server.playerDropped(source)
+-- end)
+
+---@diagnostic disable-next-line: param-type-mismatch
+AddStateBagChangeHandler('isLoggedIn', nil, function(bagName, _, value)
+	local player = GetPlayerFromStateBagName(bagName)
+	if not player then return end
+	if not value then
+		server.playerDropped(player)
+	end
+end)
